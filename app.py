1736ac34b78263cda02fd83a218acf7c64cb9b84
Marina <vakuol88@mail.ru>
2019-12-04 21:00:04 +0500

    add sql

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+from modules import db, Men
+from routes import api
 
-@app.route('/')
-def hello():
-    return 'Hello world'
+app = Flask(__name__)
+app.register_blueprint(api)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+    db.session.add(Men(name = 'Bob'))
+    db.session.add(Men(name = 'Rocky'))
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run()
---
+++ routes.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify
+from modules import Men
+
+api = Blueprint('api', __name__)
+
+@api.route('/')
+def hello():
+    return jsonify([(lambda men: men.json())(men) for men in Men.query.all()])
